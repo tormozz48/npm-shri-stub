8ba07966f4cbfc0db2ac6514681ed34b4aae0afc
tormozz48 <tormozz48@gmail.com>
2016-08-02 23:06:48 +0300

    Add source code and test for it

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('chai').assert;
+const shri = require('..');
+
+describe('npm-shri-stub', () => {
+
+    it('should calculate sum of given numbers', () => {
+        assert.equal(shri.add(1, 2), 3);
+    });
+
+    it('should calculate difference between given numbers', () => {
+        assert.equal(shri.subtract(3, 2), 1);
+    });
+
+    it('should multiply given numbers', () => {
+        assert.equal(shri.multiply(2, 3), 6);
+    });
+
+    it('should divide given numbers', () => {
+        assert.equal(shri.divide(4, 2), 2);
+    });
+});
