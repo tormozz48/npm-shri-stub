d2685790c54cbe9907a7134fdf148090f405bf56
tormozz48 <tormozz48@gmail.com>
2016-08-02 23:30:34 +0300

    Add cli interface for package

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
 'use strict';
 
-/**
- * Calculates sum of two given numbers
- * @param {Number} a
- * @param {Number} b
- */
-exports.add = (a, b) => a + b;
-
-/**
- * Calculates difference between two given numbers
- * @param {Number} a
- * @param {Number} b
- */
-exports.subtract = (a, b) => a - b;
-
-/**
- * Multiplies given numbers
- * @param {Number} a
- * @param {Number} b
- */
-exports.multiply = (a, b) => a * b;
-
-/**
- * Divides given numbers
- * @param {Number} a
- * @param {Number} b
- */
-exports.divide = (a, b) => a / b;
+module.exports = require('./lib/core');
